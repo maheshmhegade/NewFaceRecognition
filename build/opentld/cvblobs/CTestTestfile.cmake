7bcb1043486834a8ca03b92adfcfc5212201c318
maheshmhegade <maheshmhegade@gmail.com>
2013-02-06 16:27:41 +0530

    minor tweaks

--- build/opentld/cvblobs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mmh/softwares/FaceRecognition/opentld/cvblobs
-# Build directory: /home/mmh/softwares/FaceRecognition/build/opentld/cvblobs
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/opentld/tldmain/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mmh/softwares/FaceRecognition/opentld/tldmain
-# Build directory: /home/mmh/softwares/FaceRecognition/build/opentld/tldmain
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
