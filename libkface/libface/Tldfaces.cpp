084e0cf78d3c16a14af42cfef05867a0ee5d6f8a
Gilles Caulier <caulier.gilles@gmail.com>
2013-02-07 11:19:27 +0100

    add License header for new source code Fix coding style to be more readable

--- libkface/libface/Tldfaces.cpp
+++ libkface/libface/Tldfaces.cpp
@@ -1,51 +1,91 @@
+/** ===========================================================
+ * @file
+ *
+ * This file is a part of libface project
+ * <a href="http://libface.sourceforge.net">http://libface.sourceforge.net</a>
+ *
+ * @date    2010-03-03
+ * @brief   openTLD interface.
+ * @section DESCRIPTION
+ *
+ * Face detection through openTLD interface.
+ *
+ * @author Copyright (C) 2012-2013 by Mahesh Hegde
+ *         <a href="mailto:maheshmhegade at gmail dot com">maheshmhegade at gmail dot com</a>
+ * @author Copyright (C) 2013 by Gilles Caulier
+ *         <a href="mailto:caulier dot gilles at gmail dot com">caulier dot gilles at gmail dot com</a>
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
 #include "Tldfaces.h"
+
 using namespace std;
-libface::Tldface::Tldface()
+
+namespace libface
 {
-    main = new Main();
-    imAcq = imAcqAlloc();
-    gui = new Gui();
-    main->gui = gui;
+
+Tldface::Tldface()
+{
+    main        = new Main();
+    imAcq       = imAcqAlloc();
+    gui         = new Gui();
+    main->gui   = gui;
     main->imAcq = imAcq;
     config.configure(main);
     srand(main->seed);
     imAcqInit(imAcq);
 }
 
-libface::Tldface::~Tldface()
+Tldface::~Tldface()
 {
  //   delete main;
 }
 
-int libface::Tldface::writeModelTofile(IplImage *inputImage, const char *faceModelFilename)
+int Tldface::writeModelTofile(IplImage* inputImage, const char* faceModelFilename)
 {
     float dummyVariable;
-    main->doWork(inputImage,faceModelFilename,1,&dummyVariable);
+    main->doWork(inputImage, faceModelFilename, 1, &dummyVariable);
     return 0;
 }
 
-float libface::Tldface::getRecognitionConfidence(IplImage *inputImage, const char *faceModelFilename)
+float Tldface::getRecognitionConfidence(IplImage* inputImage, const char* faceModelFilename)
 {
     float recognitionConfidence;
-    main->doWork(inputImage,faceModelFilename,2,&recognitionConfidence);
+    main->doWork(inputImage, faceModelFilename, 2, &recognitionConfidence);
     return recognitionConfidence;
 }
 
-int libface::Tldface::updateDatabase(IplImage *inputImage, const char *faceModelFilename)
+int Tldface::updateDatabase(IplImage* /*inputImage*/, const char* /*faceModelFilename*/)
 {
-    //main->doWork(inputImage,faceModelFilename,3);
+    //main->doWork(inputImage, faceModelFilename, 3);
     return 0;
 }
 
-IplImage *libface::Tldface::QImage2IplImage(QImage qimg)
+IplImage* Tldface::QImage2IplImage(QImage qimg)
 {
 
-    IplImage *imgHeader = cvCreateImageHeader( cvSize(qimg.width(), qimg.height()), IPL_DEPTH_8U, 4);
+    IplImage* imgHeader  = cvCreateImageHeader(cvSize(qimg.width(), qimg.height()), IPL_DEPTH_8U, 4);
     imgHeader->imageData = (char*) qimg.bits();
 
-    uchar* newdata = (uchar*) malloc(sizeof(uchar) * qimg.byteCount());
+    uchar* newdata       = (uchar*) malloc(sizeof(uchar) * qimg.byteCount());
     memcpy(newdata, qimg.bits(), qimg.byteCount());
     imgHeader->imageData = (char*) newdata;
+
     return imgHeader;
 }
+
+} // namespace libface
 
--- libkface/libface/Tldfaces.h
+++ libkface/libface/Tldfaces.h
@@ -1,9 +1,45 @@
+/** ===========================================================
+ * @file
+ *
+ * This file is a part of libface project
+ * <a href="http://libface.sourceforge.net">http://libface.sourceforge.net</a>
+ *
+ * @date    2010-03-03
+ * @brief   openTLD interface.
+ * @section DESCRIPTION
+ *
+ * Face detection through openTLD interface.
+ *
+ * @author Copyright (C) 2012-2013 by Mahesh Hegde
+ *         <a href="mailto:maheshmhegade at gmail dot com">maheshmhegade at gmail dot com</a>
+ * @author Copyright (C) 2013 by Gilles Caulier
+ *         <a href="mailto:caulier dot gilles at gmail dot com">caulier dot gilles at gmail dot com</a>
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+// Qt includes
+
+#include <QImage>
+
+// OpenTLD includes
+
 #include "../../opentld/libopentld/ImAcq.h"
 #include "../../opentld/tldmain/Config.h"
 #include "../../opentld/tldmain/Main.h"
 #include "../../opentld/tldmain/Gui.h"
-
-#include <QImage>
 
 using namespace tld;
 using tld::Config;
@@ -12,24 +48,28 @@ using tld::Settings;
 
 namespace libface
 {
+
 class Tldface
 {
- public:
-    Main *main;
-    Config config;
-    ImAcq *imAcq;
-    Gui *gui;
-    Tldface();
+public:
 
+    Tldface();
    ~Tldface();
 
-    int updateDatabase(IplImage* ,const char *);
+    int updateDatabase(IplImage*, const char*);
 
-    float getRecognitionConfidence(IplImage* ,const char *);
+    float getRecognitionConfidence(IplImage*, const char*);
 
-    int writeModelTofile(IplImage* ,const char *);
+    int writeModelTofile(IplImage*, const char*);
 
-    IplImage* QImage2IplImage(QImage );
+    IplImage* QImage2IplImage(QImage);
 
+public:
+
+    Main*  main;
+    Config config;
+    ImAcq* imAcq;
+    Gui*   gui;
 };
-}
+
+} // namespace libface
