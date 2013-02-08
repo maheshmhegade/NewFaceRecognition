b1bd3979aa8d35b914cd8e94443fa3f0d3d182d9
Gilles Caulier <caulier.gilles@gmail.com>
2013-02-08 06:37:42 +0100

    fix warnings

--- opentld/libconfig/grammar.c
+++ opentld/libconfig/grammar.c
@@ -281,8 +281,8 @@ typedef short int yytype_int16;
 #define YYSIZE_MAXIMUM ((YYSIZE_T) -1)
 
 #ifndef YY_
-# if YYENABLE_NLS
-#  if ENABLE_NLS
+# ifdef YYENABLE_NLS
+#  ifdef ENABLE_NLS
 #   include <libintl.h> /* INFRINGES ON USER NAME SPACE */
 #   define YY_(msgid) dgettext ("bison-runtime", msgid)
 #  endif
